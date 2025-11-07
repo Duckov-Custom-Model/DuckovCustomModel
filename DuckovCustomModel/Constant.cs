861ce039ab99634c3402969be8f8a1abc0802b9e
OLC <utsumabo@ritsukage.com>
2025-11-07 19:19:22 +0800

    Bump version to 1.1.1 in Constant.cs

--- DuckovCustomModel/Constant.cs
+++ DuckovCustomModel/Constant.cs
@@ -7,7 +7,7 @@ namespace DuckovCustomModel
     {
         public const string ModID = "DuckovCustomModel";
         public const string ModName = "Duckov Custom Model";
-        public const string ModVersion = "1.1.0";
+        public const string ModVersion = "1.1.1";
         public const string HarmonyId = "com.ritsukage.DuckovCustomModel";
 
         public static readonly JsonSerializerSettings JsonSettings = new()
