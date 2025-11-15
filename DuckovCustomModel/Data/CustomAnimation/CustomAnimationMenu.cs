ab3c0e2a706d3d97e52377df348acb3ef7e7df22
OLC <utsumabo@ritsukage.com>
2025-11-15 18:15:34 +0800

    feat: add CustomAnimationMenu and CustomAnimationParameters classes for managing custom animations

---
+++ DuckovCustomModel/Data/CustomAnimation/CustomAnimationMenu.cs
@@ -0,0 +1,27 @@
+﻿namespace DuckovCustomModel.Data.CustomAnimation
+{
+    public class CustomAnimationMenu
+    {
+        public const int MaxControls = 8;
+        public Control[] Controls { get; set; } = [];
+
+        public class Control
+        {
+            public enum ControlType
+            {
+                Button,
+                Toggle,
+                SubMenu,
+                TwoAxisPuppet,
+                FourAxisPuppet,
+                RadialPuppet,
+            }
+
+            public string Name { get; set; } = string.Empty;
+            public string IconPath { get; set; } = string.Empty;
+            public ControlType Type { get; set; } = ControlType.Button;
+            public CustomAnimationMenu? SubMenu { get; set; } = null;
+            public string[]? SubParameterNames { get; set; } = null;
+        }
+    }
+}
---
+++ DuckovCustomModel/Data/CustomAnimation/CustomAnimationParameters.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DuckovCustomModel.Data.CustomAnimation
+{
+    public class CustomAnimationParameters
+    {
+        public List<ParameterData> Parameters { get; set; } = [];
+
+        public class ParameterData
+        {
+            public string Name { get; set; } = string.Empty;
+            public float DefaultValue { get; set; } = 0f;
+            public AnimatorControllerParameterType Type { get; set; } = AnimatorControllerParameterType.Float;
+        }
+    }
+}
